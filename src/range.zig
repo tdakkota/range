4218a1883f82cded82625c8ea5b22ecd7e9f1d96
tdakkota <tanc13@yandex.ru>
2020-03-19 12:40:27 +0300

    add tests, add range_step iterator

--- src/range.zig
+++ src/range.zig
@@ -7,15 +7,42 @@ const RangeIterator = struct {
 
     max: i32,
     current: i32,
+    step: i32,
 
     pub fn next(self: *Self) ?i32 {
-        if (self.current - self.max == 1) return null;
-        defer self.current += 1;
+        if (self.current - self.max >= self.step) return null;
+        defer self.current += self.step;
 
         return self.current;
     }
 };
 
 pub fn range(count: i32) RangeIterator {
-    return RangeIterator{.max = count - 1, .current = 0};
+    return RangeIterator{.max = count - 1, .current = 0, .step = 1};
+}
+
+pub fn range_iter(count: i32, step: i32) RangeIterator {
+    return RangeIterator{.max = count - 1, .current = 0, .step = step};
+}
+
+test "range" {
+    var iter = range(10);
+    while (iter.next()) |value| {
+        std.debug.warn("Current value is {}\n", value);
+    }
+}
+
+test "range_iter" {
+    const step: i32 = 2;
+    var iter = range_iter(10, step);
+
+    var prev : i32 = iter.current; 
+    var tmp = iter.next(); // skip first zero value
+    while (iter.next()) |value| {
+        std.debug.warn("Current value is {}\n", value);
+        if (value - prev != step) {
+            std.debug.panic("value should be {}, not {}", prev + step, value);
+        }
+        prev += step;
+    }
 }
